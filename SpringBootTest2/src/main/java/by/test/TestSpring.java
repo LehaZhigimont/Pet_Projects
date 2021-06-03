1ed033ca85700c65088df12a81a1fc50348f9bfc
Admin <foto-minsk@ya.ru>
2021-06-03 07:30:02 +0300

    equals beans and scope

--- SpringBootTest2/src/main/java/by/test/TestSpring.java
+++ SpringBootTest2/src/main/java/by/test/TestSpring.java
@@ -6,10 +6,19 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
-       MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondmusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
-       musicPlayer.playMusic();
-        System.out.println(musicPlayer.getName() + "\n" + musicPlayer.getVolume());
+
+        System.out.println(firstMusicPlayer == secondmusicPlayer);
+
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondmusicPlayer);
+
+        firstMusicPlayer.setVolume(10);
+
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondmusicPlayer.getVolume());
 
         context.close();
     }
